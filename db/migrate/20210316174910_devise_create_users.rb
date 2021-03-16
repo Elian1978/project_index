4ee780b54f815b04e4c77b820c2d7a194682f273
Elian Rojas <impresosgosen@gmail.com>
2021-03-16 12:48:25 -0600

    Second commit, Add User Model, welcome controller and index action

--- db/migrate/20210316174910_devise_create_users.rb
+++ db/migrate/20210316174910_devise_create_users.rb
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :username
+      t.string :username, unique: true
       t.string :profile_picture
 
       t.timestamps null: false
